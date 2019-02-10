0ac6eba00f1e3b2586b1ecf619ff11809becefeb
Cory Efird <cefird101@gmail.com>
2019-02-09 19:39:42 -0700

    Add a GameBoard that displays Cards in a css grid.

--- games/src/memory/components/GameBoard.js
+++ games/src/memory/components/GameBoard.js
@@ -3,9 +3,9 @@ import Card from './Card';
 
 const styles = {
     board : {
-        "display": "grid",
-        "margin": "auto",
-        "grid-gap": "2vh 2vh", // spacing between cards
+        display: "grid",
+        margin: "auto",
+        gridGap: "2vh 2vh", // spacing between cards
     },
 };
 
@@ -17,22 +17,36 @@ class GameBoard extends React.Component {
     render() {
 
         // Flatten the gameStates 2d board array into a 1d array of cards
+        // Also give each card its row and column as a property since its needed later
         var gameState = this.props.gameState;
         var cards = [];
         for (var row = 0; row < gameState.rows; row++) {
             for (var col = 0; col < gameState.columns; col++) {
-                cards.push(gameState.board[row][col]);
+                var card = {row:row, col:col};
+                card = Object.assign(card, gameState.board[row][col]);
+                cards.push(card);
             }
         }
 
-        // Set the size of each row and column
-        styles.board["grid-template-columns"] = "10vh ".repeat(gameState.columns);
-        styles.board["grid-template-rows"] = "10vh ".repeat(gameState.rows);
+        // Set the number of rows and columns, as well as the size
+        var boardStyle = {
+            ...styles.board,
+            gridTemplateColumns: "10vh ".repeat(gameState.columns),
+            gridTemplateRows: "10vh ".repeat(gameState.rows),
+        };
 
         // map Card components to the 1d array of cards
+        // a unique key is calculated using the row and column of each Card so that React stops complaining
         return (
-            <div style = {styles.board}>
-                {cards.map((card) => (<Card id = {card.cardID}/>))}
+            <div style = {boardStyle}>
+                {cards.map((card) => (
+                    <Card
+                        key={card.row * 10 + card.col}
+                        id={card.cardID}
+                        faceUp={card.faceUp}
+                        onClick={() => this.props.onClick(card.row, card.col)}
+                    />
+                    ))}
             </div>
         );
     }
