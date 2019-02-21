fff46dc00dcfb3b4032b439f41b9c158183bbdbf
Cory Efird <cefird101@gmail.com>
2019-02-21 00:46:39 -0700

    Add touch emulation.

--- games/src/index.js
+++ games/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
 import App from './App';
+import TouchEmulator from "./TouchEmulator";
 
+const touchEmulator = new TouchEmulator();
 
 ReactDOM.render(<App />, document.getElementById('root'));
