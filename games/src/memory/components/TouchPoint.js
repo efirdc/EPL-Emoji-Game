a52da4639d973c09f339b5ef93a983a1efc83423
Cory Efird <cefird101@gmail.com>
2019-02-25 12:49:55 -0700

    WIP: Touch point react-motion experiment

--- games/src/memory/components/TouchPoint.js
+++ games/src/memory/components/TouchPoint.js
@@ -1,27 +1,29 @@
 import React from 'react';
+import {Motion, spring} from 'react-motion';
 
 export default function TouchPoint(props) {
-    let transform = 'translate(' + (props.x - (props.size / 2)) + 'px, ' + (props.y - (props.size / 2)) + 'px)';
-    let style = {
+
+    let getStyle = (x, y, size) => { return {
         position: 'fixed',
         left: 0,
         top: 0,
         background: '#fff',
         border: 'solid 1px #999',
         opacity: .4,
-        borderRadius: '100%',
-        height: props.size + 'px',
-        width: props.size + 'px',
+        borderRadius: '50%',
+        height: size + 'px',
+        width: size + 'px',
         padding: 0,
         margin: 0,
         display: 'block',
         overflow: 'hidden',
         //pointerEvents: 'none',
         //userSelect: 'none',
-        transform: transform,
+        transform: 'translate(' + (x - (size / 2)) + 'px, ' + (y - (size / 2)) + 'px)',
         zIndex: 100
-    };
+    }};
     return (
+        <Motion defaultStyle={{x: 0, y: 0}}
         <div
             className={props.fake ? "FakeTouchPoint" : "TouchPoint"}
             style={style}
