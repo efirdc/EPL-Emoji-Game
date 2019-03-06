4a7b293d173358ae36b6fe5d7ed3cba6bedef2fb
Cory Efird <cefird101@gmail.com>
2019-03-06 12:02:54 -0700

    Reduce size of boundary hexagons.

--- games/src/memory/components/InnerCells.js
+++ games/src/memory/components/InnerCells.js
@@ -6,7 +6,7 @@ export default class InnerCells extends React.PureComponent {
             position: 'fixed',
             height: this.props.size + "vh",
             width: this.props.size + "vh",
-            transform: `translate(${x - 0.5 * this.props.size}vh, ${y - 0.5 * this.props.size}vh) scale(0.9)`,
+            transform: `translate(${x - 0.5 * this.props.size}vh, ${y - 0.5 * this.props.size}vh) scale(0.8)`,
             //zIndex: -1,
             clipPath: "polygon(50% 0%, 93.30125% 25%, 93.30125% 75%, 50% 100%, 6.69875% 75%, 6.69875% 25%)",
             backgroundColor : "#fbfdff"
