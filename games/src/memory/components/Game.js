1b2a2bebd9b576241c1b61034841430960cfe425
Cory Efird <cefird101@gmail.com>
2019-03-07 12:34:24 -0700

    Fix bug where timer goes down during loading phase.

--- games/src/memory/components/Game.js
+++ games/src/memory/components/Game.js
@@ -33,8 +33,7 @@ export default class Game extends React.Component {
         this.loop = new GameLoop();
 
         this.gameLogic = new GameLogic();
-        this.gameLogic.setLevel(22);
-        this.gameLogic.startLevel();
+        this.gameLogic.setLevel(50);
 
         this.touchedCards = [];
         this.touchPoints = {}; // Touch points created by real touch events
