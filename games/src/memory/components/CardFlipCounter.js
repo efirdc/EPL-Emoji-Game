9809d267d0f1ad92bb5a99588fcb202627cf242a
Cory Efird <cefird101@gmail.com>
2019-03-25 12:50:55 -0600

    Change cardflipcounter to be a % filled circle.

--- games/src/memory/components/CardFlipCounter.js
+++ games/src/memory/components/CardFlipCounter.js
@@ -1,30 +1,81 @@
 import React from 'react';
 import "./Fonts.css"
+import {Motion, spring, presets} from 'react-motion';
+export default class CardFlipCounter extends React.Component {
 
-export default function CardFlipCounter(props) {
-    let counterStyle = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        position: 'absolute',
-        height: 0,
-        width: 0,
-        transform: `translate(${props.x}vh, ${props.y}vh) rotate(${props.rotation}deg)`,
-        zIndex: 1
-    };
+    containerStyle() {
+        return {
+            zIndex: 2,
+            position: 'absolute',
+            transform: `
+                translate(${this.props.x}vh, ${this.props.y}vh) 
+                rotate(${this.props.rotation}deg)
+            `,
+        }
+    }
 
-    let flipsLeft = props.maxFlips - props.numFlips;
+    mainCircleStyle() {
+        let radius = 4.5;
+        return {
+            zIndex: 2,
+            position: 'absolute',
+            width: (radius * 2) + "vh",
+            height: (radius * 2) + "vh",
+            borderRadius: "50%",
+            borderWidth: "0.5vh",
+            borderColor: "#16162f",
+            borderStyle: "solid",
+            backgroundColor: "#e5eae8",
+            transform: `
+                translate(${-radius}vh, ${-radius}vh) 
+            `
+        }
+    }
 
-    return (
-        <div style={counterStyle}>
-            <h1
-                className={"hullFont"}
-                style={{
-                    fontSize: "7vh",
-                }}
+    innerCircleStyle(fillPercent) {
+        let radius = 4.0;
+        return {
+            zIndex: 2,
+            width: (radius * 2) + "vh",
+            height: (radius * 2) + "vh",
+            position: 'absolute',
+            borderRadius: '50%',
+            backgroundColor: "#009cff",
+            transform: `
+                translate(${-radius}vh, ${-radius}vh) 
+                scale(${fillPercent})
+            `
+        }
+    }
+
+    render() {
+        let numberStyle = {
+            fontSize: "7vh",
+            position: "absolute",
+            transform: "translate(-50%, -50%)",
+            zIndex: 2,
+        };
+
+        let flipsLeft = this.props.maxFlips - this.props.numFlips;
+        let flipPercent = flipsLeft / this.props.maxFlips;
+
+        return (
+            <Motion
+                defaultStyle={{flipPercent}}
+                style={{flipPercent: spring(flipPercent, presets.wobbly)}}
             >
-                {flipsLeft}
-            </h1>
-        </div>
-    );
+                {interpolatingStyle => {
+                return (
+                    <div style={this.containerStyle()}>
+                        <div style={this.mainCircleStyle()}/>
+                        <div style={this.innerCircleStyle(interpolatingStyle.flipPercent)}/>
+                        <h1 className={"hullFont"} style={numberStyle}>
+                            {this.props.maxFlips}
+                        </h1>
+                    </div>
+                )
+                }}
+            </Motion>
+        );
+    }
 }
