227e77b5a08da844bac92d39e089daf2a88a1f61
Cory Efird <cefird101@gmail.com>
2019-03-26 10:32:36 -0600

    Add an indicator for adding time when the clock is matched (WIP)

--- games/src/memory/components/Timer.js
+++ games/src/memory/components/Timer.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import "./Fonts.css"
+import {Motion, presets, spring} from "react-motion";
+import CardFlipDot from "./CardFlipCounter";
 
 export default class Timer extends React.Component {
+
+    timerIndicatorTextStyle(scale, rotation) {
+        let size = 3;
+        return {
+            zIndex: '2',
+            position: 'absolute',
+            transform: `
+                translate(-50%, -50%) 
+                rotate(${rotation}deg) 
+                scale(${scale})
+            `,
+            fontFamily: "'Arial Black', Gadget, sans-serif",
+            fontSize: size + "vh",
+            WebkitTextStrokeWidth: 0.2 + 'vh',
+            WebkitTextStrokeColor: "black",
+            lineHeight: size + "vh",
+        }
+    }
 
     render() {
 
         let gameLogic = this.props.gameLogic;
 
         let containerStyle = {
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
             position: 'absolute',
             height: 0,
             width: 0,
@@ -18,7 +35,15 @@ export default class Timer extends React.Component {
                 translate(${this.props.x}vh, ${this.props.y}vh) 
                 rotate(${this.props.rotation}deg)
             `,
-            zIndex: 1,
+            zIndex: 2,
+        };
+
+        let numberStyle = {
+            fontSize: "7vh",
+            position: "absolute",
+            transform: "translate(-50%, -50%)",
+            hyphens: "none",
+            zIndex: 2,
         };
 
         let minutes, seconds;
@@ -30,18 +55,45 @@ export default class Timer extends React.Component {
             seconds = Math.floor(time - minutes * 60).toString().padStart(2, '0');
         }
 
+        let timerIndicatorScale = 0.0;
+        let timeSinceTimerMatched = Date.now() - this.props.timeAtTimerMatched;
+        if (gameLogic.wizardMatched && timeSinceTimerMatched < 4000) {
+            timerIndicatorScale = 1.0;
+        }
+
+        let timerIndicatorContainerStyle = {
+            zIndex: '2',
+            position: 'absolute',
+            width: '0vh',
+            height: '0vh',
+            transform: `translate(0vh, -8vh)`
+        };
+
         return (
             <div style={containerStyle}>
                 <h1
                     className={"hullFont"}
-                    style={{
-                        fontWeight: "200",
-                        fontSize: "7vh",
-                        hyphens: "none",
-                    }}
+                    style={numberStyle}
                 >
                     {minutes + ":" + seconds}
                 </h1>
+                <Motion
+                    defaultStyle={{scale: 0.0}}
+                    style={{scale: spring(timerIndicatorScale, presets.stiff)}}
+                >
+                    {interpolatingStyle => {
+                        return (
+                            <div style={timerIndicatorContainerStyle}>
+                                <h1
+                                    style={this.timerIndicatorTextStyle(interpolatingStyle.scale, 0.0)}
+                                >
+                                    {"⏱ + " + gameLogic.level.timerAdds}
+                                </h1>
+
+                            </div>
+                        )
+                    }}
+                </Motion>
             </div>
         );
     }
