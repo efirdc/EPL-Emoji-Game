7cfa15da012c9809cab1400cb486cef156782f6f
kira-m <kira.muhlbauer@gmail.com>
2019-04-03 08:30:31 -0600

    fixed package.json

--- games/Memory-win32-x64/resources/app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import TouchEmulator from "./TouchEmulator";
-
-// Comment this to disable touch emulation
-//const touchEmulator = new TouchEmulator();
-
-ReactDOM.render(<App />, document.getElementById('root'));
