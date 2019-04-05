378217a1214cb32b15df24688aea54ee2784c5e8
Cory Efird <cefird101@gmail.com>
2019-04-05 12:15:31 -0600

    Add Router thing to make gh-pages work.

--- games/src/App.js
+++ games/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Game from './memory/components/Game';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Game/>
-      </div>
+        <Route exact path={`/`} render={ (routerProps) =>
+            <div  className="App">
+              <Game routerProps={routerProps}/>
+            </div>
+        }
+        />
     );
   }
 }
